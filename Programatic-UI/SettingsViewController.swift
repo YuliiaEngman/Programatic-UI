dc4f4e28b79b564d6dbd042065fce29149bf024e
Yuliia Engman <yuliia.engman@gmail.com>
2020-01-28 15:58:59 -0500

    completed project

--- Programatic-UI/SettingsViewController.swift
+++ Programatic-UI/SettingsViewController.swift
@@ -13,6 +13,7 @@ import UIKit
 // STEP 1 of
 struct AppKey {
     static let appColorKey = "app color"
+    static let indexForRow = ""
 }
 
 class SettingsViewController: UIViewController {
@@ -36,8 +37,20 @@ class SettingsViewController: UIViewController {
         
         
         // TODO: get UserDefaults
-        let colorName = colors[0] //hard coding the first index
-        view.backgroundColor = UIColor(named: colorName)
+        
+        //let colorName = colors[UserDefaults.standard]
+        //let colorName = colors[0]
+        //hard coding the first index
+       // view.backgroundColor = UIColor(named: colorName)
+       savedColorsAndRow()
+    }
+    
+    func savedColorsAndRow() {
+        if let colorName = UserDefaults.standard.object(forKey: AppKey.appColorKey) as? String, let row = UserDefaults.standard.object(forKey: AppKey.indexForRow) as? Int {
+            view.backgroundColor = UIColor(named: colorName)
+            settingsView.pickerView.selectRow(row, inComponent: 0, animated: true)
+            // inComponent relates to pickerView and let's keep it simple = 0
+        }
     }
 }
 
@@ -64,6 +77,25 @@ extension SettingsViewController: UIPickerViewDelegate {
         //USER DEFAULTS
         // STEP 2 of  (Next steps inside MainViewController)
         UserDefaults.standard.set(colorName, forKey: AppKey.appColorKey)
-        
+        UserDefaults.standard.set(row, forKey: AppKey.indexForRow)
     }
 }
+
+
+/*
+ In terms to retrieve the collor of UserDefaults as well as row for picker view we needed:
+1. create a new variable in UserDefaults that will catch Integer for the row (we need Int because to get row in pickerview we will use this code, that requires Int for row - settingsView.pickerView.selectRow(row, inComponent: 0, animated: true)
+2. In step 2 save row of pickerview to User Defaults
+3. Retrieve row and color from UserDefaults using func savedColorsAndRow():
+func savedColorsAndRow() {
+       if let colorName = UserDefaults.standard.object(forKey: AppKey.appColorKey) as? String, let row = UserDefaults.standard.object(forKey: AppKey.indexForRow) as? Int {
+           view.backgroundColor = UIColor(named: colorName)
+           settingsView.pickerView.selectRow(row, inComponent: 0, animated: true)
+           // inComponent relates to pickerView and let's keep it simple = 0
+       }
+   }
+ in this function we downcasting row as type Any to Int
+ 
+ DONE!
+ */
+
